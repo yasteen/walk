04f35a62aab6b4ed322f4308818d3d6937e7847e
yasteen <justinchan135@gmail.com>
2021-01-08 00:37:10 -0800

    Reimplemented using new functionality in creatures.py

--- creatures.py
+++ creatures.py
@@ -5,26 +5,21 @@ from typing import Dict, Tuple, Callable
 
 def human(space : pymunk.Space, position : Tuple[int, int]) -> Creature:
     size = (40, 100)
-    person = Creature(40, size, position)
+    person = Creature(40, size, position, "rectangle")
     armsize = (16, 80)
-    arm1 = person.addLimb(10, armsize, (0,  -armsize[1] / 2), (0, -size[1] / 4), True, True)
-    arm2 = person.addLimb(10, armsize, (0,  -armsize[1] / 2), (0, -size[1] / 4), False, True)
+    arm1 = person.addLimb(10, armsize, (0,  -armsize[1] / 2), (0, -size[1] / 4), True, True, "rectangle")
+    arm2 = person.addLimb(10, armsize, (0,  -armsize[1] / 2), (0, -size[1] / 4), False, True, "rectangle")
     thighsize = (20, 40)
-    thigh1 = person.addLimb(10, thighsize, (0, -thighsize[1] / 2), (0, size[1] / 2), True, True)
-    thigh2 = person.addLimb(10, thighsize, (0, -thighsize[1] / 2), (0, size[1] / 2), False, True)
+    thigh1 = person.addLimb(10, thighsize, (0, -thighsize[1] / 2), (0, size[1] / 2), True, True, "rectangle")
+    thigh2 = person.addLimb(10, thighsize, (0, -thighsize[1] / 2), (0, size[1] / 2), False, True, "rectangle")
     shinsize = (15, 30)
-    shin1 = thigh1.addLimb(5, shinsize, (0, -shinsize[1] / 2), (0, thighsize[1] / 2), True, True)
-    shin2 = thigh2.addLimb(5, shinsize, (0, -shinsize[1] / 2), (0, thighsize[1] / 2), False, True)
-
-    person.create(space)
+    shin1 = thigh1.addLimb(5, shinsize, (0, -shinsize[1] / 2), (0, thighsize[1] / 2), True, True, "rectangle")
+    shin2 = thigh2.addLimb(5, shinsize, (0, -shinsize[1] / 2), (0, thighsize[1] / 2), False, True, "rectangle")
 
     headRadius = 30
-    head = pymunk.Body(12, pymunk.moment_for_circle(12, 0, headRadius))
-    head.position = (position[0], position[1] - size[1] / 2)
-    headShape = pymunk.Circle(head, headRadius)
-    headShape.filter = pymunk.ShapeFilter(group = 0x1)
-    headPivot = pymunk.PivotJoint(person.body, head, (position[0], position[1] - size[1] / 2))
-    space.add(head, headShape, headPivot)
+    head = person.addLimb(12, (0, 30), (0,0), (0, -size[1] / 2), False, False, "circle")
+
+    person.create(space)
     return person
     
 
