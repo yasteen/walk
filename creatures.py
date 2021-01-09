ca8193a8e544021ba4d595f4387f65478e69b730
yasteen <justinchan135@gmail.com>
2021-01-09 09:53:13 -0800

    Added dog

--- creatures.py
+++ creatures.py
@@ -17,12 +17,36 @@ def human(space : pymunk.Space, position : Tuple[int, int]) -> Creature:
     shin2 = thigh2.addLimb(5, shinsize, (0, -shinsize[1] / 2), (0, thighsize[1] / 2), False, True, "rectangle")
 
     headRadius = 30
-    head = person.addLimb(12, (0, 30), (0,0), (0, -size[1] / 2), False, False, "circle")
+    head = person.addLimb(12, (0, headRadius), (0,0), (0, -size[1] / 2), False, False, "circle")
 
     person.create(space)
     return person
     
+def dog(space: pymunk.Space, position : Tuple[int, int]) -> Creature:
+    size = (100, 40)
+    dog = Creature(40, size, position, "rectangle")
+    legsize = (15, 20)
+    leg1 = dog.addLimb(10, legsize, (0, -legsize[1] / 2), (-size[0]/2+legsize[0]/2, size[1]/2), True, True, "rectangle")
+    leg2 = dog.addLimb(10, legsize, (0, -legsize[1] / 2), (-size[0]/2+legsize[0]/2, size[1]/2), False, True, "rectangle")
+
+    leg3 = dog.addLimb(10, legsize, (0, -legsize[1] / 2), (size[0]/2-legsize[0]/2, size[1]/2), True, True, "rectangle")
+    leg4 = dog.addLimb(10, legsize, (0, -legsize[1] / 2), (size[0]/2-legsize[0]/2, size[1]/2), False, True, "rectangle")
+
+    lleg1 = leg1.addLimb(10, legsize, (0, -legsize[1] / 2), (0, legsize[1] / 2), True, True, "rectangle")
+    lleg2 = leg2.addLimb(10, legsize, (0, -legsize[1] / 2), (0, legsize[1] / 2), False, True, "rectangle")
+
+    lleg3 = leg3.addLimb(10, legsize, (0, -legsize[1] / 2), (0, legsize[1] / 2), True, True, "rectangle")
+    lleg4 = leg4.addLimb(10, legsize, (0, -legsize[1] / 2), (0, legsize[1] / 2), False, True, "rectangle")
+
+
+
+    headRadius = 30
+    head = dog.addLimb(12, (0, headRadius), (0, 0), (size[0] / 2, -size[1] / 2), False, False, "circle")
+
+    dog.create(space)
+    return dog
+
 
     ## DICTIONARY ##
 
-creatures : Dict[str, Callable[[pymunk.Space, Tuple[int, int]], Creature]] = {"human" : human}
+creatures : Dict[str, Callable[[pymunk.Space, Tuple[int, int]], Creature]] = {"human" : human, "dog" : dog}
